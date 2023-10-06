e6b8654895d3f7de8b7247a114f88fb7f09b0787
JamesonGG <95173860+JamesonGG@users.noreply.github.com>
2023-10-07 00:28:30 +1100

    Saved Files

---
+++ Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    private float damage;
+    private float protection;
+    private float weight;
+    private float value;
+    private bool isWeapon;
+    public bool IsWeapon
+    {
+        get { return isWeapon; }
+    }
+}
